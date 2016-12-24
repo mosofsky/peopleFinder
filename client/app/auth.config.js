b0432fe00f972e7de9efea204242b6dbad26d0bb
Michael Osofsky <michael@uluvit.com>
2016-12-24 14:55:51 -0800

    Auth0 Initial Checkin, not tested

---
+++ client/app/auth.config.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.myConfig = {
+    clientID: 'GA9Lm1gungRvQdtvnMFJ3tDJLGoPJJ2q',
+    domain: 'mosofsky.auth0.com'
+};
+//# sourceMappingURL=auth.config.js.map
---
+++ client/app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'GA9Lm1gungRvQdtvnMFJ3tDJLGoPJJ2q',
+    domain: 'mosofsky.auth0.com'
+};
