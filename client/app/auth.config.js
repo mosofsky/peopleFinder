58d80e6ac3ef2fe475c984136a9fc228d815ee6e
Michael Osofsky <michael@uluvit.com>
2016-12-24 15:14:18 -0800

    Removed Auth0 and will re-add to retry

--- client/app/auth.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.myConfig = {
-    clientID: 'GA9Lm1gungRvQdtvnMFJ3tDJLGoPJJ2q',
-    domain: 'mosofsky.auth0.com'
-};
-//# sourceMappingURL=auth.config.js.map
--- client/app/auth.config.ts
+++
@@ -1,9 +0,0 @@
-interface AuthConfiguration {
-    clientID: string,
-    domain: string
-}
-
-export const myConfig: AuthConfiguration = {
-    clientID: 'GA9Lm1gungRvQdtvnMFJ3tDJLGoPJJ2q',
-    domain: 'mosofsky.auth0.com'
-};
