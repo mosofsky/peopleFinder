761f3148d94f6e3a28bc69e81e4d145eea5002a9
Michael Osofsky <michael@uluvit.com>
2016-12-24 16:21:29 -0800

    Added 'Get Handles' button and tested it gets called

---
+++ client/app/instagramService.service.ts
@@ -0,0 +1,13 @@
+import { Injectable }      from '@angular/core';
+
+@Injectable()
+export class InstagramService {
+
+  constructor() {
+
+  }
+
+  public getHandlesMentioningHashtag() {
+
+  };
+}
