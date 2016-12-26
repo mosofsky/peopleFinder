2f908cbf70edd9b8f8bfbfab0ddbebae351df4be
Michael Osofsky <michael@uluvit.com>
2016-12-26 09:34:26 -0800

    Renamed python script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,3 @@
 python client.py
+
+echo "Load this URL in your browser https://www.instagram.com/oauth/authorize/?client_id=1748a14705504707a7c7108b249d4dbe&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code"
